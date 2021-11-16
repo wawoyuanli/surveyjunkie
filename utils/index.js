f9c72437ee3a961e14bf531d4c74a120ada32773
wawoyuanli <1583649818@qq.com>
2021-11-16 17:09:18 +0800

    index.js

--- utils/index.js
+++ utils/index.js
@@ -1,22 +1,9 @@
-// import $ from 'jquery'
-function initIframeHeight(height, id) {
-    var userAgent = navigator.userAgent;
-    var iframe = parent.document.getElementById(id);
-    var subdoc = iframe.contentDocument || iframe.contentWindow.document;
-    var subbody = subdoc.body;
-    var realHeight;
-    //谷歌浏览器特殊处理
-    if (userAgent.indexOf("Chrome") > -1) {
-        realHeight = subdoc.documentElement.scrollHeight;
-    } else {
-        realHeight = subbody.scrollHeight;
-    }
-    if (realHeight < height) {
-        $(iframe).height(height);
-    } else {
-        $(iframe).height(realHeight);
-    }
+
+function collapseHandle(tabId,contentId) { 
+    document.getElementById(tabId).addEventListener('click', function () {
+        document.getElementById(contentId).style.display = "none";
+    })
 }
-export {
-    initIframeHeight
+export { 
+    collapseHandle
 }
