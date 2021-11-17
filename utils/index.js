549280861d87a2afcf681867a91cebb3e3ae95cb
wawoyuanli <1583649818@qq.com>
2021-11-17 15:52:36 +0800

    update

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,42 @@
-
-function collapseHandle(tabId,contentId) { 
-    document.getElementById(tabId).addEventListener('click', function () {
-        document.getElementById(contentId).style.display = "none";
+/**给元素标签添加类 */
+function addClass(tabId, contentId) {
+    var _tabId = document.getElementById(tabId)
+    var _contentId = document.getElementById(contentId)
+    _tabId.onmouseover = function () {
+        _contentId.classList.add('curent_active_show')
+    }
+    // _tabId.onmouseout = function () {
+    //     _contentId.classList.remove('curent_active_show')
+    // }
+}
+function removeClass(contentId) {
+    document.getElementById(contentId).onmouseover = function () {
+        this.classList.add('curent_active_show')
+    }
+    document.getElementById(contentId).onmouseout = function () {
+        this.classList.remove('curent_active_show')
+    }
+}
+/**点击显示提示 */
+function showToast(id) {
+    var toastid = document.getElementById(id)
+    toastid.addEventListener('click', function () {
+        alert('敬请期待')
     })
 }
-export { 
-    collapseHandle
+/**点击跳转 */
+function route(id, url) {
+    var _id = document.getElementById(id)
+    _id.addEventListener('click', function () {
+        window.location.href = url
+    })
+
+}
+
+/**导出 */
+export {
+    addClass,
+    removeClass,
+    showToast,
+    route
 }
