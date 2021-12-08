e28c20cdf4aa200a6b6fff5f369e713b340f682d
wawoyuanli <1583649818@qq.com>
2021-12-08 09:55:50 +0800

    修改utils 文件路径

--- gulpfile.js
+++ gulpfile.js
@@ -11,44 +11,44 @@ const revCollector = require('gulp-rev-collector')
 const connect = require('gulp-connect')
 
 gulp.task('handleHtml', function () {
-    return src('src/*.html').pipe(dest('prodbuild/dist'))
+    return src('src/*.html').pipe(dest('dist'))
 })
 gulp.task('pages', function () {
-    return src('src/pages/**/*.html')
-        .pipe(dest('prodbuild/dist/pages'))
+    return src('src/pages/**')
+        .pipe(dest('dist/pages'))
 })
 /**压缩less */
 gulp.task('minLess', function () {
     return gulp.src('src/assets/styles/*.less')
         .pipe(less())
-        .pipe(gulp.dest('prodbuild/dist/less'))
+        .pipe(gulp.dest('dist/assets/styles'))
         .pipe(connect.reload())
 });
 /**压缩css */
 gulp.task('mincss', function () {
     return src('src/assets/styles/*.css')
         .pipe(gulpCssmin()) //   .pipe(concat('main.min.css'))
-        .pipe(dest('prodbuild/dist/assets/styles'))
+        .pipe(dest('dist/assets/styles'))
         .pipe(connect.reload())
 })
 /**压缩js */
 gulp.task('minjs', function () {
-    return src('./utils/*.js')
+    return src('src/utils/*.js')
         .pipe(gulpUglify()) //  .pipe(concat('main.min.js'))
-        .pipe(dest('prodbuild/utils'))
+        .pipe(dest('dist/utils'))
         .pipe(connect.reload())
 })
 /**压缩image*/
 gulp.task('minImage', function () {
     return src('src/assets/images/*') //所有图片
         .pipe(gulpImagem({ progressive: true }))
-        .pipe(dest('prodbuild/dist/assets/images'))
+        .pipe(dest('dist/assets/images'))
         .pipe(connect.reload())
 })
 /**lib */
 gulp.task('copylib', function () {
-    return src('lib/**/*')
-        .pipe(dest('prodbuild/lib'))
+    return src('src/lib/**')
+        .pipe(dest('dist/lib'))
         .pipe(connect.reload())
 })
 
@@ -58,37 +58,33 @@ gulp.task('watchFileChange', async () => {
     gulp.watch('src/*.html', gulp.task('concatHtml'))
     gulp.watch('src/assets/styles/*.less', gulp.task('minLess'))
     gulp.watch('src/assets/styles/*.css', gulp.task('mincss'))
-    gulp.watch('./utils/*.js', gulp.task('minjs'))
+    gulp.watch('src/utils/*.js', gulp.task('minjs'))
     gulp.watch('src/assets/images/*', gulp.task('minImage'))
 })
 
 /**清空dist文件夹 */
 gulp.task('clean', function () {
-    return gulp.src(['prodbuild/dist/*'])
+    return gulp.src(['dist/*'])
         .pipe(clean());
 });
 
 /**压缩 */
 gulp.task('zip', function () {
-    return src('prodbuild/dist').pipe()
+    return src('dist').pipe()
 })
 /**启动服务器 */
 gulp.task("connect", function () {
     console.log('---启动服务---')
     connect.server({
-        root: "prodbuild/dist",
+        root: "dist",
         livereload: true,
         port: '8030'
     });
 });
 
 gulp.task('rev', function () {
-    return gulp.src(['src/pages/**/*.html', 'src/pages/**/**/*.html'], { base: 'src' })
+    return gulp.src(['src/pages/**'], { base: 'src' })
         .pipe(revCollector({ replaceReved: true }))
-        .pipe(gulp.dest('prodbuild/dist/pages'));
+        .pipe(gulp.dest('dist/pages'));
 });
-
-
-
-
-exports.default = series('handleHtml', 'copylib', 'mincss', 'minjs', 'minImage', 'pages', 'rev', 'watchFileChange','connect')
+exports.default = parallel('handleHtml', 'copylib', 'mincss', 'minjs', 'minImage', 'pages', 'watchFileChange','connect')
