03e6d10d462dc9a85d86916e01e08271b20c9ae1
wawoyuanli <1583649818@qq.com>
2021-12-14 14:28:04 +0800

    update

--- src/utils/login.js
+++ src/utils/login.js
@@ -1,60 +1,35 @@
-
-function loginbtn() { 
+// import { requestAjax} from './request.js'
+/**登陆接口 */
+function login() { 
   if (!valideInput('username')) {
     alert("请输入用户名");
   }
   if (!valideInput("password")) {
     alert("请输入密码");
   }
+  requestAjax({
+    method: 'POST',
+    url: "/manager/authorization/sys/login",
+    data: {
+      username: 'hyl',
+      password:'123456'
+    }
+  }, function (res) { 
+    console.log(res)
+  })
   var ajaxObj = new XMLHttpRequest();
   ajaxObj.open('POST', '/manager/authorization/sys/login')
   ajaxObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   ajaxObj.send('username=hyl&password=123456');
   ajaxObj.onreadystatechange = function () {
-    // 为了保证 数据 完整返回，我们一般会判断 两个值
     if (ajaxObj.readyState == 4 && ajaxObj.status == 200) {
-      // 如果能够进到这个判断 说明 数据 完美的回来了,并且请求的页面是存在的
-      // 5.在注册的事件中 获取 返回的 内容 并修改页面的显示
       console.log('数据返回成功');
-      // 数据是保存在 异步对象的 属性中
       console.log(ajaxObj.responseText);
     }
   }
 }
-document.getElementById('login_btn').addEventListener('click',loginbtn())
 // 校验
 function valideInput(id) { 
  return document.getElementById(id).value
 }
-// (function () { 
-//   function loginbtn() { 
-//     debugger
-//     document.getElementById("login_btn").addEventListener("click", function () {
-//       if (!valideInput('username')) {
-//         alert("请输入用户名");
-//       }
-//       if (!valideInput("password")) {
-//         alert("请输入密码");
-//       }
-//       var ajaxObj = new XMLHttpRequest();
-//       ajaxObj.open('POST', '/manager/authorization/sys/login')
-//       ajaxObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-//       ajaxObj.send('username=hyl&password=123456');
-//       ajaxObj.onreadystatechange = function () {
-//         // 为了保证 数据 完整返回，我们一般会判断 两个值
-//         if (ajaxObj.readyState == 4 && ajaxObj.status == 200) {
-//           // 如果能够进到这个判断 说明 数据 完美的回来了,并且请求的页面是存在的
-//           // 5.在注册的事件中 获取 返回的 内容 并修改页面的显示
-//           console.log('数据返回成功');
-//           // 数据是保存在 异步对象的 属性中
-//           console.log(ajaxObj.responseText);
-//         }
-//       }
-//     });
-//   }
-//   //校验
-//   function valideInput(id) { 
-//    return document.getElementById(id).value
-//   }
-// })()
-// window.onload=loginbtn()
+ 
