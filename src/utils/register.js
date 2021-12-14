26d560f2bcd38be3cebe862963a76e0d3d82db22
wawoyuanli <1583649818@qq.com>
2021-12-14 15:40:00 +0800

    footer update

--- src/utils/register.js
+++ src/utils/register.js
@@ -1,34 +1,37 @@
 /**注册接口 */
-function registerHandler() { 
-    if (!valideInput('username')) {
-      alert("请输入用户名");
-    }
-    if (!valideInput("password")) {
-      alert("请输入密码");
-    }
-    requestAjax({
-      method: 'POST',
-      url: "/manager/authorization/sys/login",
-      data: {
-        username: 'hyl',
-        password:'123456'
-      }
-    }, function (res) { 
-      console.log(res)
-    })
-    var ajaxObj = new XMLHttpRequest();
-    ajaxObj.open('POST', '/manager/authorization/sys/login')
-    ajaxObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    ajaxObj.send('username=hyl&password=123456');
-    ajaxObj.onreadystatechange = function () {
-      if (ajaxObj.readyState == 4 && ajaxObj.status == 200) {
-        console.log('数据返回成功');
-        console.log(ajaxObj.responseText);
-      }
+function registerHandler() {
+  if (!valideInput('username')) {
+    alert("请输入用户名");
+  }
+  if (!valideInput("password")) {
+    alert("请输入密码");
+  }
+  var ajaxObj = new XMLHttpRequest();
+  ajaxObj.open('POST', '/manager/authorization/sys/register/100')
+  ajaxObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  ajaxObj.send('username=hyl&password=123456');
+  ajaxObj.onreadystatechange = function () {
+    if (ajaxObj.readyState == 4 && ajaxObj.status == 200) {
+      console.log('数据返回成功');
+      console.log(ajaxObj.responseText);
     }
   }
-  // 校验
-  function valideInput(id) { 
-   return document.getElementById(id).value
-  }
-   
+}
+// 校验
+function valideInput(id) {
+  return document.getElementById(id).value
+}
+// $("#registerbtn").click(function () {
+//   requestAjax({
+//     url: "/manager/authorization/sys/register/100",
+//     async: true,
+//     method: "POST",
+//     data: {
+//       username: getInputvalueById("username"),
+//       password: getInputvalueById("passworSd"),
+//     },
+//     success: function (data) {
+//       console.log(data);
+//     },
+//   });
+// }); 
