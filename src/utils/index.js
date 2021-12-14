8edf6040f310b788a93d3f24cc1865f89e8462c4
wawoyuanli <1583649818@qq.com>
2021-12-14 11:31:49 +0800

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,33 +1,33 @@
 /**给元素标签添加类 */
-export function addClass(tabId, contentId) {
-  var _tabId = document.getElementById(tabId);
-  var _contentId = document.getElementById(contentId);
-  _tabId.onmouseover = function () {
-    _contentId.classList.add("curent_active_show");
-  };
-}
-export function removeClass(contentId) {
-  document.getElementById(contentId).onmouseover = function () {
-    this.classList.add("curent_active_show");
-  };
-  document.getElementById(contentId).onmouseout = function () {
-    this.classList.remove("curent_active_show");
-  };
-}
-/**点击显示提示 */
-export function showToast(id) {
-  var toastid = document.getElementById(id);
-  toastid.addEventListener("click", function () {
-    alert("敬请期待");
-  });
-}
-/**点击跳转 */
-export function route(id, url) {
-  var _id = document.getElementById(id);
-  _id.addEventListener("click", function () {
-    window.location.href = url;
-  });
-}
+// export function addClass(tabId, contentId) {
+//   var _tabId = document.getElementById(tabId);
+//   var _contentId = document.getElementById(contentId);
+//   _tabId.onmouseover = function () {
+//     _contentId.classList.add("curent_active_show");
+//   };
+// }
+// export function removeClass(contentId) {
+//   document.getElementById(contentId).onmouseover = function () {
+//     this.classList.add("curent_active_show");
+//   };
+//   document.getElementById(contentId).onmouseout = function () {
+//     this.classList.remove("curent_active_show");
+//   };
+// }
+// /**点击显示提示 */
+// export function showToast(id) {
+//   var toastid = document.getElementById(id);
+//   toastid.addEventListener("click", function () {
+//     alert("敬请期待");
+//   });
+// }
+// /**点击跳转 */
+// export function route(id, url) {
+//   var _id = document.getElementById(id);
+//   _id.addEventListener("click", function () {
+//     window.location.href = url;
+//   });
+// }
 /**导出 */
 // export {
 //     addClass,
@@ -35,3 +35,11 @@ export function route(id, url) {
 //     showToast,
 //     route
 // }
+ const name = 'hyl'
+// const name='hyl'
+// module.exports = {
+//   name
+// }
+export { 
+  name
+}
